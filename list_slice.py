5135f7f5026ad13329440ae19a6564eb2f2e50be
adharsh-18 <85212328+adharsh-18@users.noreply.github.com>
2021-07-26 10:22:17 +0530

    Create list_slice.py

---
+++ list_slice.py
@@ -0,0 +1,13 @@
+#list creation
+l = ["USA","INDIA","BRAZIL","ARGENTINA","CANADA","AUSTRALIA","SOUTH AFRICA","UK"]
+
+#for slicing use list_name[start_index:end_index:step_index]
+
+#straight slicing
+print(l[2:6:1])
+print(l[:4:1]) #blank values are taken to be the first or last
+
+#reverse slicing
+
+print(l[-1::-1])
+print(l[::-1])
